482faa1cebd8de7e8acd1452761e57b8c6cdb968
rishabhkhare314 <31706963+rishabhkhare314@users.noreply.github.com>
2020-04-13 17:13:06 +0530

    Create name.js

---
+++ name.js
@@ -0,0 +1,35 @@
+[
+    {
+       
+        "firstName": "Hardik",
+        "lastName": "Motwani",
+       
+    },
+    {
+  
+        "firstName": "Captain",
+        "lastName": "America",
+    },
+    {
+    
+        "firstName": "Iron",
+        "lastName": "Man",
+  
+    },
+    {
+          
+            "firstName": "Black",
+            "lastName": "Panther",
+            
+          },
+          {
+        
+            "firstName": "Iron",
+            "lastName": "Man",
+            
+          },
+          {
+            "firstName": "Spider",
+            "lastName": "Man",
+          }
+]
